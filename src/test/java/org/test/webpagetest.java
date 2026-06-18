7828236c31e4ca5184ee6c72425df1cbf6563435
srushtisru76 <srushtisru76@gmail.com>
2026-06-18 09:10:42 +0530

    Add JUnit test

--- src/test/java/org/test/webpagetest.java
+++ src/test/java/org/test/webpagetest.java
@@ -1,4 +1,12 @@
 package org.test;
 
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
 public class webpagetest {
+
+    @Test
+    public void testSample() {
+        assertTrue(true);
+    }
 }
